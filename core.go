a3f1b7044a3d6506490a48ba1e419fad7b9e79c7
cn-maul <155143652+cn-maul@users.noreply.github.com>
2024-02-20 14:42:27 +0800

    new file

--- core.go
+++ core.go
@@ -1,6 +1,7 @@
 package tinylog
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"strings"
@@ -14,6 +15,7 @@ type Logger struct {
 	Prefix string           //前缀
 	Format string           //支持结构化日志（如JSON格式）
 	Buf    *strings.Builder //暂时存储日志队列
+	BufNum int              //对列允许存储的最大消息数量计数器
 }
 
 var DefaultLogger = Logger{
@@ -21,6 +23,7 @@ var DefaultLogger = Logger{
 	Prefix: DefaultPrefix,
 	Format: "",
 	Buf:    new(strings.Builder),
+	BufNum: 0,
 }
 
 func New() *Logger {
@@ -31,4 +34,31 @@ func New() *Logger {
 
 	DefaultLogger.Output = output
 	return &DefaultLogger
+}
+
+func (l *Logger) reset() {
+	l.BufNum = 0
+	l.Buf.Reset()
+}
+
+func (l *Logger) Println(logLevel, msg string) {
+	var midPrefix string
+	switch logLevel {
+	case "log":
+		midPrefix = " [Msg]"
+	case "err":
+		midPrefix = " [Err]"
+	default:
+		midPrefix = " [Nil]"
+	}
+
+	if l.BufNum == 10 {
+		//写入文件的函数
+		l.reset()
+	}
+
+	logMsg := l.Prefix + midPrefix + msg + "\n"
+	fmt.Print(logMsg)
+	l.Buf.Grow(len(logMsg))
+	l.Buf.WriteString(logMsg)
 }
