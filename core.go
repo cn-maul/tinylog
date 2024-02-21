e4d6df02a9c6f740efd6afeaa7528c1d29f161d2
cn-maul <155143652+cn-maul@users.noreply.github.com>
2024-02-21 21:24:16 +0800

    fix bugs

--- core.go
+++ core.go
@@ -3,23 +3,19 @@ package tinylog
 import (
 	"log"
 	"os"
-	"sync"
 	"time"
 )
 
 var (
-	OutFormat     = 0
-	DefaultPrefix = time.Now().Format("2006/01/02 15:04:05")
-	OutputPrefix  = time.Now().Format("2006-01-02 15-04-05")
+	OutFormat = 0
+	Prefix    = time.Now().Format("2006/01/02 15:04:05")
+	Filename  = time.Now().Format("2006-01-02 15-04-05")
 )
 
 type Logger struct {
 	Prefix string // 日志前缀，用于标识日志的类型或来源
-	mode   int    // 日志格式，如JSON，指定日志输出的结构
 	count  int    // 日志缓冲区中允许存储的最大消息数量
 	queue  []LogMsg
-	mu     sync.Mutex // 互斥锁，确保并发写入日志时的线程安全
-	wg     sync.WaitGroup
 	Output *os.File // 日志文件输出流，用于将日志写入到文件
 }
 
@@ -29,23 +25,13 @@ type LogMsg struct {
 	msg   string
 }
 
-var DefaultLogger = Logger{
-	Prefix: DefaultPrefix,
-	mode:   0,
-	count:  0,
-	queue:  make([]LogMsg, 0),
-	mu:     sync.Mutex{},
-	wg:     sync.WaitGroup{},
-	Output: nil,
-}
-
 // New 方法用于创建一个新的Logger实例，并初始化其输出文件。
 func New() *Logger {
-	// 设置DefaultLogger的输出文件为新创建的文件
-	DefaultLogger.Output = file()
-
-	// 返回DefaultLogger的指针
-	return &DefaultLogger
+	return &Logger{
+		Prefix: Prefix,
+		count:  0,
+		queue:  make([]LogMsg, 0),
+	}
 }
 
 // Log 方法用于记录一条日志消息，支持不同的日志级别。
@@ -63,11 +49,11 @@ func (l *Logger) Log(level int, msg string) {
 	}
 
 	// 构造完整的日志消息
-	logtext := l.Prefix + mid + msg + "\n"
-	log.Print(logtext) // 将日志消息直接打印到控制台
+	content := l.Prefix + mid + msg + "\n"
+	log.Print(content) // 将日志消息直接打印到控制台
 
 	logmsg := &LogMsg{
-		time:  DefaultPrefix,
+		time:  Prefix,
 		level: level,
 		msg:   msg,
 	}
@@ -76,23 +62,7 @@ func (l *Logger) Log(level int, msg string) {
 
 	// 如果缓冲区中的日志消息数量达到或超过10条，调用Flush方法将它们写入文件
 	if l.count >= 10 {
-		l.wg.Add(1)
-		go l.Flush()
+		l.Output = open()
+		l.Flush()
 	}
-}
-
-// Flush 方法用于将Logger缓冲区中的所有日志消息写入到日志文件中。
-func (l *Logger) Flush() {
-	l.mu.Lock()
-	defer l.mu.Unlock()
-
-	switch l.mode {
-	case 0:
-		l.writeLog()
-	case 1:
-		l.writeJSON()
-	default:
-		log.Println("Flush时mode出错")
-	}
-	l.wg.Wait()
 }
--- os.go
+++ os.go
@@ -2,12 +2,17 @@ package tinylog
 
 import (
 	"bufio"
-	"encoding/json"
-	"fmt"
+	"io"
 	"log"
 	"os"
 	"strings"
+
+	jsoniter "github.com/json-iterator/go"
 )
+
+type jsonData struct {
+	Data []LogMsg
+}
 
 var (
 	Level = map[int]string{
@@ -20,30 +25,23 @@ var (
 	}
 )
 
-// Write 方法用于将Logger缓冲区中的日志消息写入到文件，并刷新缓冲区
-func (l *Logger) write(buf string) {
-	// 创建一个带有缓冲的写入器，提高写入效率
-	writer := bufio.NewWriter(l.Output)
-
-	// 尝试将缓冲区的内容写入文件
-	_, err := writer.WriteString(buf)
+func write(output io.Writer, input string) {
+	buf := bufio.NewWriter(output)
+	_, err := buf.WriteString(input)
 	if err != nil {
-		log.Println("Bufio写时出错:", err)
+		log.Println(err)
 		return
 	}
 
-	// 刷新写入器，确保所有数据都被写入文件
-	err = writer.Flush()
+	err = buf.Flush()
 	if err != nil {
-		log.Println("Flush时出错:", err)
+		log.Println(err)
 		return
 	}
 }
 
-// checkOutput 方法用于检查日志输出目录是否存在，并在需要时创建它。
-func checkOutput() {
-	logDir := "./log" // 日志输出目录的路径
-
+func open() *os.File {
+	logDir := "./log"
 	// 检查日志目录是否存在
 	if _, err := os.Stat(logDir); os.IsNotExist(err) {
 		// 如果目录不存在，创建它
@@ -51,39 +49,53 @@ func checkOutput() {
 		// 创建目录并设置适当的权限
 		if err = os.MkdirAll(logDir, 0o755); err != nil {
 			log.Println("创建log目录失败:", err)
+			return nil
 		}
 	}
+
+	filePath := "./log/" + Filename + Mode[OutFormat]
+	file, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0o644)
+	if err != nil { // 如果创建文件失败，记录错误信息
+		log.Println(err)
+	}
+	return file
 }
 
 func (l *Logger) writeLog() {
 	var buf strings.Builder
-
 	for _, v := range l.queue {
 		content := l.Prefix + Level[v.level] + v.msg + "\n"
 		buf.Grow(len(content))
 		buf.WriteString(content)
 	}
-	l.write(buf.String())
-
+	write(l.Output, buf.String())
 	// 重置缓冲区，准备接收新的日志消息
 	l.count = 0
 	l.queue = l.queue[:0]
-	defer l.wg.Done()
 }
 
 func (l *Logger) writeJSON() {
-	// 将切片转换为JSON字符串
-	jsonBytes, err := json.Marshal(l.queue)
-	if err != nil {
-		fmt.Println("JSON编码出错:", err)
-		return
+	data := jsonData{
+		Data: l.queue,
 	}
-	l.write(string(jsonBytes))
-
+	json := jsoniter.ConfigCompatibleWithStandardLibrary
+	jsonBytes, _ := json.Marshal(&data)
+	write(l.Output, string(jsonBytes))
 	// 重置缓冲区，准备接收新的日志消息
 	l.count = 0
 	l.queue = l.queue[:0]
-	defer l.wg.Done()
+}
+
+// Flush 方法用于将Logger缓冲区中的所有日志消息写入到日志文件中。
+func (l *Logger) Flush() {
+	switch OutFormat {
+	case 0:
+		l.writeLog()
+	case 1:
+		l.writeJSON()
+	default:
+		log.Println("Flush时mode出错")
+	}
 }
 
 func SetMode(mode string) {
@@ -95,15 +107,4 @@ func SetMode(mode string) {
 	default:
 		log.Println("未知Mode")
 	}
-}
-
-func file() *os.File {
-	checkOutput() // 检查并创建日志输出目录
-
-	// 创建日志文件，文件名由当前时间的日期格式和".log"后缀组成
-	output, err := os.Create("./log/" + OutputPrefix + Mode[OutFormat])
-	if err != nil { // 如果创建文件失败，记录错误信息
-		log.Println(err)
-	}
-	return output
 }
