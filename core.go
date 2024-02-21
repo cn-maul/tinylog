0155984c8d6aab6135ee1e4856d80b9692440276
cn-maul <155143652+cn-maul@users.noreply.github.com>
2024-02-21 18:39:15 +0800

    新增了日志的json格式输出

--- core.go
+++ core.go
@@ -3,49 +3,46 @@ package tinylog
 import (
 	"log"
 	"os"
-	"strings"
 	"sync"
 	"time"
 )
 
-const (
-	InfoLevel  = "inf"
-	ErrorLevel = "err"
+var (
+	OutFormat     = 0
+	DefaultPrefix = time.Now().Format("2006/01/02 15:04:05")
+	OutputPrefix  = time.Now().Format("2006-01-02 15-04-05")
 )
 
-var DefaultPrefix = time.Now().Format("2006/01/02 15:04:05")
-var OutputPrefix = time.Now().Format("2006-01-02 15-04-05")
-
 type Logger struct {
-	Prefix string           // 日志前缀，用于标识日志的类型或来源
-	Format string           // 日志格式，如JSON，用于指定日志输出的结构
-	BufNum int              // 日志缓冲区中允许存储的最大消息数量
-	Buf    *strings.Builder // 日志缓冲区，用于暂存日志消息，以优化性能
-	mu     sync.Mutex       // 互斥锁，确保并发写入日志时的线程安全
-	Output *os.File         // 日志文件输出流，用于将日志写入到文件
+	Prefix string // 日志前缀，用于标识日志的类型或来源
+	mode   int    // 日志格式，如JSON，指定日志输出的结构
+	count  int    // 日志缓冲区中允许存储的最大消息数量
+	queue  []LogMsg
+	mu     sync.Mutex // 互斥锁，确保并发写入日志时的线程安全
+	wg     sync.WaitGroup
+	Output *os.File // 日志文件输出流，用于将日志写入到文件
+}
+
+type LogMsg struct {
+	time  string
+	level int
+	msg   string
 }
 
 var DefaultLogger = Logger{
 	Prefix: DefaultPrefix,
-	Format: "",
-	BufNum: 0,
-	Buf:    new(strings.Builder),
+	mode:   0,
+	count:  0,
+	queue:  make([]LogMsg, 0),
 	mu:     sync.Mutex{},
+	wg:     sync.WaitGroup{},
 	Output: nil,
 }
 
 // New 方法用于创建一个新的Logger实例，并初始化其输出文件。
 func New() *Logger {
-	checkOutput() // 检查并创建日志输出目录
-
-	// 创建日志文件，文件名由当前时间的日期格式和".log"后缀组成
-	output, err := os.Create("./log/" + OutputPrefix + ".log")
-	if err != nil { // 如果创建文件失败，记录错误信息
-		log.Println(err)
-	}
-
 	// 设置DefaultLogger的输出文件为新创建的文件
-	DefaultLogger.Output = output
+	DefaultLogger.Output = file()
 
 	// 返回DefaultLogger的指针
 	return &DefaultLogger
@@ -53,50 +50,49 @@ func New() *Logger {
 
 // Log 方法用于记录一条日志消息，支持不同的日志级别。
 // level 参数指定日志的级别，msg 是要记录的消息内容。
-func (l *Logger) Log(level, msg string) {
+func (l *Logger) Log(level int, msg string) {
+	mid := ""
 	// 根据日志级别设置中间前缀
-	midPrefix := ""
 	switch level {
-	case InfoLevel:
-		midPrefix = " [Inf]"
-	case ErrorLevel:
-		midPrefix = " [Err]"
+	case 0:
+		mid = " [Inf]"
+	case 1:
+		mid = " [Err]"
 	default:
-		midPrefix = " [Unk]"
-	}
-
-	// 如果缓冲区中的日志消息数量达到或超过10条，调用Flush方法将它们写入文件
-	if l.BufNum >= 10 {
-		l.Flush()
+		mid = " [Unk]"
 	}
 
 	// 构造完整的日志消息
-	logMsg := l.Prefix + midPrefix + msg + "\n"
-	log.Print(logMsg) // 将日志消息直接打印到控制台
+	logtext := l.Prefix + mid + msg + "\n"
+	log.Print(logtext) // 将日志消息直接打印到控制台
 
-	// 将日志消息追加到缓冲区
-	l.Buf.Grow(len(logMsg))   // 确保缓冲区有足够的空间
-	l.Buf.WriteString(logMsg) // 写入日志消息
-	l.BufNum++                // 更新缓冲区中的消息计数
+	logmsg := &LogMsg{
+		time:  DefaultPrefix,
+		level: level,
+		msg:   msg,
+	}
+	l.queue = append(l.queue, *logmsg)
+	l.count++ // 更新缓冲区中的消息计数
+
+	// 如果缓冲区中的日志消息数量达到或超过10条，调用Flush方法将它们写入文件
+	if l.count >= 10 {
+		l.wg.Add(1)
+		go l.Flush()
+	}
 }
 
 // Flush 方法用于将Logger缓冲区中的所有日志消息写入到日志文件中。
 func (l *Logger) Flush() {
-	l.mu.Lock()         // 获取互斥锁，确保并发安全
-	defer l.mu.Unlock() // 释放互斥锁
+	l.mu.Lock()
+	defer l.mu.Unlock()
 
-	// 尝试将缓冲区的内容写入日志文件
-	_, err := l.Output.WriteString(l.Buf.String())
-	if err != nil { // 如果写入失败，记录错误信息
-		log.Println(err)
+	switch l.mode {
+	case 0:
+		l.writeLog()
+	case 1:
+		l.writeJSON()
+	default:
+		log.Println("Flush时mode出错")
 	}
-
-	// 重置缓冲区，准备接收新的日志消息
-	l.reset()
-}
-
-// reset 方法用于重置Logger的缓冲区，清除所有已存储的日志消息。
-func (l *Logger) reset() {
-	l.BufNum = 0  // 重置缓冲区计数器
-	l.Buf.Reset() // 清空缓冲区内容
+	l.wg.Wait()
 }
--- os.go
+++ os.go
@@ -2,17 +2,31 @@ package tinylog
 
 import (
 	"bufio"
+	"encoding/json"
+	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
-// Write 方法用于将Logger缓冲区中的日志消息写入到文件，并刷新缓冲区。
-func (l *Logger) Write() {
+var (
+	Level = map[int]string{
+		0: "[Inf]",
+		1: "[Err]",
+	}
+	Mode = map[int]string{
+		0: ".log",
+		1: ".json",
+	}
+)
+
+// Write 方法用于将Logger缓冲区中的日志消息写入到文件，并刷新缓冲区
+func (l *Logger) write(buf string) {
 	// 创建一个带有缓冲的写入器，提高写入效率
 	writer := bufio.NewWriter(l.Output)
 
 	// 尝试将缓冲区的内容写入文件
-	_, err := writer.WriteString(l.Buf.String())
+	_, err := writer.WriteString(buf)
 	if err != nil {
 		log.Println("Bufio写时出错:", err)
 		return
@@ -34,9 +48,62 @@ func checkOutput() {
 	if _, err := os.Stat(logDir); os.IsNotExist(err) {
 		// 如果目录不存在，创建它
 		log.Println("日志文件夹不存在，正在创建log目录")
-		err = os.MkdirAll(logDir, 0755) // 创建目录并设置适当的权限
-		if err != nil {
-			log.Println("创建日志目录失败:", err)
+		// 创建目录并设置适当的权限
+		if err = os.MkdirAll(logDir, 0o755); err != nil {
+			log.Println("创建log目录失败:", err)
 		}
 	}
+}
+
+func (l *Logger) writeLog() {
+	var buf strings.Builder
+
+	for _, v := range l.queue {
+		content := l.Prefix + Level[v.level] + v.msg + "\n"
+		buf.Grow(len(content))
+		buf.WriteString(content)
+	}
+	l.write(buf.String())
+
+	// 重置缓冲区，准备接收新的日志消息
+	l.count = 0
+	l.queue = l.queue[:0]
+	defer l.wg.Done()
+}
+
+func (l *Logger) writeJSON() {
+	// 将切片转换为JSON字符串
+	jsonBytes, err := json.Marshal(l.queue)
+	if err != nil {
+		fmt.Println("JSON编码出错:", err)
+		return
+	}
+	l.write(string(jsonBytes))
+
+	// 重置缓冲区，准备接收新的日志消息
+	l.count = 0
+	l.queue = l.queue[:0]
+	defer l.wg.Done()
+}
+
+func SetMode(mode string) {
+	switch mode {
+	case "text":
+		OutFormat = 0
+	case "json":
+		OutFormat = 1
+	default:
+		log.Println("未知Mode")
+	}
+}
+
+func file() *os.File {
+	checkOutput() // 检查并创建日志输出目录
+
+	// 创建日志文件，文件名由当前时间的日期格式和".log"后缀组成
+	output, err := os.Create("./log/" + OutputPrefix + Mode[OutFormat])
+	if err != nil { // 如果创建文件失败，记录错误信息
+		log.Println(err)
+	}
+	return output
 }
